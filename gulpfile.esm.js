c2a37a3eaaabf16f7c8f8824127ed627649ff0dd
Tosho <toshoajibade@gmail.com>
2019-05-16 02:00:49 +0100

    initial commit

---
+++ gulpfile.esm.js
@@ -0,0 +1,61 @@
+import sass from "gulp-sass";
+import browserSync from "browser-sync";
+import plumber from "gulp-plumber";
+import notify from "gulp-notify";
+import htmlclean from "gulp-htmlclean";
+import concat from "gulp-concat";
+import babel from "gulp-babel";
+import minimize from "gulp-uglify";
+import { src, watch, dest, series, parallel } from "gulp";
+
+browserSync.create();
+
+const css = () => {
+  return src("./src/css/*.scss")
+    .pipe(
+      plumber({
+        errorHandler: notify.onError("there is an error in sass file")
+      })
+    )
+    .pipe(sass())
+    .pipe(dest("dist/css"))
+    .pipe(browserSync.stream());
+};
+
+const html = () => {
+  return src("./src/html/*.html")
+    .pipe(htmlclean())
+    .pipe(dest("dist"))
+    .pipe(browserSync.reload({ stream: true }));
+};
+
+const javascript = () => {
+  return src("./src/js/*.js")
+    .pipe(
+      babel({
+        presets: ["@babel/env"]
+      })
+    )
+    .pipe(concat("main.js"))
+    .pipe(minimize())
+    .pipe(dest("dist"))
+    .pipe(browserSync.reload({ stream: true }));
+};
+
+const watchServer = () => {
+  watch("./src/css/*.scss", css);
+  watch("./src/js/*.js", javascript);
+  watch("./src/html/*.html", html);
+};
+
+const runServer = () => {
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  });
+};
+
+const serve = parallel(watchServer, runServer);
+
+export default serve;
---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+const boilerplate = "This is gulp es6 boilerplate"
+const hello = document.querySelector(".hello");
+hello.textContent = boilerplate;
